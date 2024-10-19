af46c6f2f422c6d9bb0f546f7e5d33995926613b
simoncwang <wang.c.simon@gmail.com>
2024-10-19 00:00:25 -0400

    updates 10/18

--- javascript/util.js
+++ javascript/util.js
@@ -14,3 +14,26 @@ function closeNav() {
     document.getElementById("expandBtn").style.display = "block";
     // document.getElementById("expandBtn").style.top=document.getElementById("topnav").offsetHeight;
 }
+
+function toggleSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    var currentDisplay = window.getComputedStyle(section).display;
+
+    if (currentDisplay === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
+
+function switchContent(currentID, nextID) {
+    var curr = document.getElementById(currentID);
+    var next = document.getElementById(nextID);
+    if (curr) {
+        curr.style.display = "none";
+    }
+
+    if (next) {
+        next.style.display = "block";
+    }
+}
